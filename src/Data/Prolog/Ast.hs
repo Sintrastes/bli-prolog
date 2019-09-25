cf7cf401381b6bad88f2cf946cc1f301226cd513
Nathan Bedell <nbedell@tulane.edu>
2019-09-25 17:12:35 -0400

    Added some types to the Ast for dealing with the new bli prolog features.

--- src/Data/Prolog/Ast.hs
+++ src/Data/Prolog/Ast.hs
@@ -13,14 +13,39 @@ module Data.Prolog.Ast where
 
 import Language.Haskell.TH.Lift
 
-type Goal = [Term]
-type LambdaGoal = ([Variable],[Term])
-type Program = Clauses
-type Clauses = [Clause]
-type Clause = (Term, Terms) -- head and body
+
+
 data Term = Var Variable
           | Comp Atom Terms
           deriving (Eq, Show, Read, Lift)
 type Terms = [Term]
 type Atom = String
 type Variable = String
+
+type Goal = [Term]
+-- A goal bound within a lambda abstraction.
+type LambdaGoal = ([Variable],[Term])
+
+type Clause = (Term, Terms) -- head and body
+
+-- A data type for bli prolog commands, which can either
+-- be a query, or an assertion. Note that here,
+-- a plain query/assertion such as 
+--
+--   person(nate). 
+--
+-- Is interpreted as a degenerate clause, i.e.
+--
+--   person(nate) :- .
+--
+
+data BliCommand = QueryMode Clause 
+                | AssertionMode Clause
+
+type Program = Clauses
+type Clauses = [Clause]
+
+-- A type synonym for BliPrograms,
+-- which can contain both assertions and queries.
+-- (This may or may not be needed).
+type BliProgram = [BliCommand]
