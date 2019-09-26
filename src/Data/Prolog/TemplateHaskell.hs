eaafe2508d64a6bfc085ba03bb7cd7fc00955eae
Nathan Bedell <nbedell@tulane.edu>
2019-09-25 20:38:40 -0400

    Started writing quasiquoters. Need to refactor names of some of the parsers.

--- src/Data/Prolog/TemplateHaskell.hs
+++ src/Data/Prolog/TemplateHaskell.hs
@@ -1,6 +1,23 @@
 {-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
 
-module Data.Prolog.TemplateHaskell where
+module Data.Prolog.TemplateHaskell(
+  goal
+) where
+
+import Data.Prolog.AST
+import Prolog.Parser as P
+import Language.Haskell.TH
+import Language.Haskell.TH.Quote
+
+goal :: QuasiQuoter
+goal = QuasiQuoter {
+    quoteExp  = undefined
+  , quotePat  = notHandled "patterns"
+  , quoteType = notHandled "types"
+  , quoteDec  = notHandled "declarations"
+  }
+  where notHandled things = error $
+            things ++ " are not handled by the regex quasiquoter."
 
 
 
