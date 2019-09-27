17830653ba7250c0985f60a70ca2bfd2d75cb154
Nathan Bedell <nbedell@tulane.edu>
2019-09-27 14:42:51 -0400

    Added stub to server.

--- src/Bli/App/Server.hs
+++ src/Bli/App/Server.hs
@@ -5,9 +5,12 @@
 
 module Bli.App.Server where
 
+import Network.Shed.Httpd
+import qualified Data.ByteString.Lazy.UTF8 as B
+
 -- | A data type to model the types of 
 --   requests that can be made to the server
-data Request = 
+data BliRequest = 
 -- | A simple Get request to 
 --   make a query and return the
 --   results of that query.
@@ -17,6 +20,18 @@ data Request =
 
 -- | A data type to model the types of responses
 --   that the server can return to clients.
-data Response = 
+data BliResponse = 
 -- | Respond with the appropriate JSON data.
-  Response String
+  BliResponse String
+
+-- Note: I'll want to wrap this in my own datatypes above,
+-- and then translate it to the "Request" and "Response" datatypes.
+requestHandler :: Request -> IO Response
+requestHandler = case reqMethod r of
+  "GET"     -> return Response { resCode = 0, resHeaders = [], resBody = B.toString $ "test" }
+  "POST"    -> return Response { resCode = 0, resHeaders = [], resBody = B.toString $ "test" }
+  otherwise -> return Response { resCode = 0, resHeaders = [], resBody = B.toString $ "test" }
+
+-- | Initialize a new bli-prolog server on @port@. 
+newServer :: Int -> IO ()
+newServer port = initServer port requestHandler
