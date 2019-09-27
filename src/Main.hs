ad0dadcc0499f2cc345a318361e38c6b4c35db3d
Nathan Bedell <nbedell@tulane.edu>
2019-09-27 12:28:16 -0400

    Addded comments to Main.hs. Fixed issue with error handling.

--- src/Main.hs
+++ src/Main.hs
@@ -21,11 +21,16 @@ import System.Console.Readline
 import Data.List.Split
 import System.Console.CmdArgs as CA hiding (program)
 
-processUserInput :: String -> Bli ()
-processUserInput input = do
+processBliCommand :: String -> Bli ()
+processBliCommand input' = do
+          -- Get schema, clauses, and options from context.
           schema <- getSchema
           clauses <- getProgram
           opts <- getOpts
+          -- Prepend the user input with the required symbol for
+          -- our parsers
+          let input = "?- " ++ input'
+          -- Parse and handle the command
           let command = P.parseBliCommand input
           case command of   
               Right x@(AssertMode goal) -> do
@@ -80,8 +85,14 @@ processUserInput input = do
                             else return ()
                          _  -> do
                             io $ mapM_ print solutions
+                     Left (A.AtomsNotInSchema atoms) -> do
+                      io $ putStrLn $ "\27[31m"++"Failure."++"\27[37m"++" Query unsuccessful."
+                      io $ putStrLn $ "    The identifiers "++ show atoms
+                      io $ putStrLn $ "    have not been declared in a schema."
+                   -- This case should not be possible since we are not dealing with a
+                   -- lambda query.
                      Left _ -> do
-                       io $ putStrLn $ "\27[31m"++"Invalid query."++"\27[37m"++" "
+                       io $ error $ "Invalid exception encountered."
               Left err -> do io $ putStrLn ("\27[31m"++"Error"++"\27[37m"++" parsing query string:")
                              io $ putStrLn $ foldr1 (\x -> \y -> x ++ "\n" ++ y) $
                                                (map (\x -> "  " ++ x)) $ 
@@ -95,6 +106,7 @@ repl = do
     Nothing -> repl
     Just line -> do
       case line of 
+        -- First,handle user REPL commands (beginning with a semicolon).
         ":h"   -> do 
           io $ putStrLn replHelpScreen
           repl
@@ -105,24 +117,29 @@ repl = do
           | isPrefixOf ":export" line -> do
                io $ putStrLn "\27[33mExport command not implemented.\27[37m"
                repl
+        -- If the user has not entered a REPL command, try processing
+        -- their input as a standard Bedelibry Prolog command.
           | otherwise -> do
-                processUserInput ("?- "++line)
+                processBliCommand line
                 repl
 
 main = do
   -- Get the version from the cabal file at compile-time.
   let v = $(getVersionFromCabal)
+  -- Make sure version loaded from file successfully.
   case v of 
     Nothing -> putStrLn $ "\27[31m"++"Error loading version info from cabal file. Aborting."++"\27[37m"
     Just version -> do
       opts <- cmdArgs $ startOptions version
-      -- If file not specified, start with an empty set of clauses.
+      -- If prolog file not specified, start with an empty set of clauses.
       p <- case program opts of
         "" -> return $ Right []
         _  -> P.clausesFromFile $ program opts
+      -- If Schema file not specified, start with an empty schema. 
       s <- case schema opts of
         "" -> return $ Right []
         _  -> schemaFromFile $ schema opts
+      -- Handle parse errors for prolog and schema files.
       case (p,s) of 
         (Left err,_) -> do putStrLn ("\27[31m"++"Error"++"\27[37m"++" parsing prolog file:") 
                            putStrLn $ foldr1 (\x -> \y -> x ++ "\n" ++ y) $
@@ -132,11 +149,16 @@ main = do
                            putStrLn $ foldr1 (\x -> \y -> x ++ "\n" ++ y) $
                                              (map (\x -> "  " ++ x)) $ 
                                              (splitOn "\n" $ show err)
+        -- If all files parse sucessfully...
         (Right clauses, Right schema) ->
           case goal opts of
             "" -> do
+               -- Print the main banner if options set to verbose.
                if (verbose opts) then putStrLn $ replBanner version else return ()
+               -- Run a bli prolog REPL with the user configuration.
                runBli opts clauses schema $ repl
-            input -> runBli opts clauses schema $ processUserInput input
+            -- If the user supplies a non-empty goal-string, run a single
+            -- command rather than starting the REPL.
+            input -> runBli opts clauses schema $ processBliCommand input
 
 
