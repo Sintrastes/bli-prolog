9a0477139d3071eae59b2e62982b8e1b5be3eca5
Nathan Bedell <nbedell@tulane.edu>
2019-09-29 15:02:27 -0400

    (Started to) add some instances to the BliSet typeclass.
    Note that I think our current interface is too general, and needs to be revised. For instance,
    our collection for the schema should not just implement efficent lookups of schema entries, but
    also permit efficently telling if a value with a certain arity (or, with a predicate matching
    a certain arity) exists in our schema. And in that case we need a more general "lookup" as part of
    the interface.
    
    Also, the datatype that is the most efficent for our collection of schema entries, and the datatype
    that is the most efficent for our collection of facts may not be the same, so our "generic" monads
    should actually be generic over both of these container types.
    
    Moreover, the interface for these two containers may in fact be different.
    
    Thus, before I try to move the main code base into this generic interface, I should
    think more about what the interface for these two container types should be.

--- src/Data/BliSet.hs
+++ src/Data/BliSet.hs
@@ -2,17 +2,54 @@
 module Data.BliSet where
 
 import Data.List
+import Data.Set (Set)
+import Data.HashSet (HashSet)
+import qualified Data.HashSet as HashSet
+import qualified Data.Set as Set
+import Data.Sequence (Seq(..), (|>), (!?), dropWhileL, findIndexL)
+import qualified Data.Sequence as Seq
 
 -- | Generic interface for a Set-like container.
 class BliSet t where
-  insert :: Eq a => a -> t a -> t a
+  empty :: t a
+  tryInsert :: Eq a => a -> t a -> Either (t a) (t a)
   tryRemove :: Eq a => a -> t a -> Either (t a) (t a)
-  lookup :: Eq a => a -> t a -> Maybe a
+  lookup :: Eq a => (a -> Bool) -> t a -> Maybe a
 
 instance BliSet [] where
-  insert x xs = nub $ xs ++ [x]
+  empty = []
+  tryInsert x xs
+      | x `elem` xs = Left xs
+      | otherwise = Right $ xs ++ [x]
   tryRemove x xs 
       | length xs' == length xs = Left xs
       | otherwise = Right xs
     where xs' = filter (\y -> y /= x) xs
-  lookup x xs = find (\y -> y == x) xs
+  lookup pred xs = find pred xs
+
+-- Another example instance of BliSet (although, I don't think this is going to be
+-- any more efficent than the list implementation.
+instance BliSet Seq where
+  empty = Empty
+  tryInsert x xs = case findIndexL (\y -> x == y) xs of 
+      (Just _) -> Left xs
+      Nothing  -> Right $ xs |> x
+  lookup pred xs = (findIndexL pred xs) >>= (xs !?)
+  tryRemove x xs
+     | length xs' == length xs = Left xs
+     | otherwise = Right xs'
+   where xs' = dropWhileL (\y -> x == y) xs
+
+instance BliSet Set where
+  empty = Set.empty
+
+-- Note: A hash set needs a hashable instance.
+--       This is probably not what we want anyway.
+{-
+instance BliSet HashSet where
+  empty = HashSet.empty
+  tryInsert x xs = case HashSet.member x xs of
+      True  -> Left  $ xs
+      False -> Right $ HashSet.insert x xs
+  lookup pred xs = undefined
+-}
