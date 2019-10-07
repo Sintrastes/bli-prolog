767f18577298f1847088060c0ea4a7040f14205d
Nathan Bedell <nbedell@tulane.edu>
2019-10-07 16:02:13 -0400

    Started refactoring the bli monads to use record syntax.

--- src/Control/Monad/Bli/Generic.hs
+++ src/Control/Monad/Bli/Generic.hs
@@ -1,5 +1,23 @@
 
-module Control.Monad.Bli.Generic where
+module Control.Monad.Bli.Generic(
+  runBli,
+  getConfig,
+  getFacts,
+  getRelations,
+  getEntities,
+  getTypes,
+  getAliases,
+  modifyConfig,
+  modifyFacts,
+  modifyRelations,
+  modifyEntities,
+  modifyTypes,
+  setConfig,
+  setFacts,
+  setRelations,
+  setEntities,
+  setTypes,
+  setAliases) where
 
 -- | Generic version of the Bli monad
 
@@ -10,40 +28,28 @@ import Data.Schema
 import Bli.App.Config
 import Data.BliSet
 
+data BliStore t1 t2 t3 t4 alias = BliStore {
+  config :: AppConfig,
+  facts  :: t1 Clause,
+  relations :: t2 RelDecl,
+  entities :: t3 EntityDecl,
+  types :: t4 TypeDecl,
+  aliases :: alias
+}
+
 type Bli t1 t2 t3 t4 alias a = 
- StateT (
-   -- Application configuration data
-   AppConfig, 
-   -- List of asserted predicates and rules
-   t1 Clause, 
-   -- List of all relations in the schema.
-   t2 RelDecl,
-   -- List of all terms in the schema.
-   t3 EntityDecl,
-   -- List of all types in the schema
-   t4 TypeDecl,
-   -- Interface for getting all type aliases. I'm not sure how this should be implemented,
-   alias
- ) IO a
+ StateT (BliStore t1 t2 t3 t4 alias) IO a
 
 -- | Lift io computations into the Bli monad.
-io :: (BliSet t1,
-       BliSet t2,
-       BliSet t3,
-       BliSet t4,
-       Alias alias)
+--   NOTE: We should just be able to use liftIO here instead, from the 
+--         MonadIO class.
+io :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
  => IO a 
  -> Bli t1 t2 t2 t3 t4 alias a
 io = lift
 
-
-
 -- | Run a Bli application with some initial state.
-runBli :: (BliSet t1,
-           BliSet t2,
-           BliSet t3,
-           BliSet t4,
-           Alias alias)
+runBli :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
   => AppConfig
   -> t1 Clause
   -> t2 RelDecl
@@ -52,118 +58,88 @@ runBli :: (BliSet t1,
   -> alias
   -> Bli t1 t2 t3 t4 alias a
   -> IO a
-runBli options program relns ents types aliases app =
-  evalStateT app (options, program, relns, ents, types, aliases)
+runBli config facts relns ents types aliases app =
+  evalStateT app (BliStore options facts relns ents types aliases)
 
--- | Get the options from a running bli application
-getOpts    :: (BliSet t1,
-               BliSet t2,
-               BliSet t3,
-               BliSet t4,
-               Alias alias)
- => Bli t1 t2 t3 t4 alias AppConfig
-getOpts = (\(x,y,z,_,_,_) -> x) <$> get
+-- | Get the configuration data from a running bli application
+getConfig :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
+  => Bli t1 t2 t3 t4 alias AppConfig
+getConfig = config <$> get
 
--- | Get the program from a running bli application.
-getProgram :: (BliSet t1,
-               BliSet t2,
-               BliSet t3,
-               BliSet t4,
-               Alias alias) => Bli t1 t2 t3 t4 alias (t1 Clause)
-getProgram = (\(x,y,z,_,_,_) -> y) <$> get
+-- | Get the currently stored facts from a running bli application.
+getFacts :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
+  => Bli t1 t2 t3 t4 alias (t1 Clause)
+getFacts = facts <$> get
 
 -- | Get the schema from a running bli application.
-getRelations  :: (BliSet t1,
-                  BliSet t2,
-                  BliSet t3,
-                  BliSet t4,
-                  Alias alias)
+getRelations :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
  => Bli t1 t2 t3 t4 alias (t2 RelDecl)
-getRelations = (\(x,y,z,_,_,_) -> z) <$> get
+getRelations = relations <$> get
 
-getEntities :: (BliSet t1,
-                BliSet t2,
-                BliSet t3,
-                BliSet t4,
-                Alias alias)
+getEntities :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
  => Bli t1 t2 t3 t4 alias (t3 EntityDecl)
-getEntities = (\(_,_,_,x,_,_) -> x) <$> get
+getEntities = entities <$> get
 
-getTypes :: (BliSet t1,
-             BliSet t2,
-             BliSet t3,
-             BliSet t4,
-             Alias alias)
+getTypes :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
  => Bli t1 t2 t3 t4 alias (t4 TypeDecl)
-getTypes = (\(_,_,_,_,x,_) -> x) <$> get
+getTypes = types <$> get
 
-getAliases :: (BliSet t1,
-               BliSet t2,
-               BliSet t3,
-               BliSet t4,
-               Alias alias)
+getAliases :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
  => Bli t1 t2 t3 t3 t4 alias (t4 TypeDecl)
-getAliases = (\(_,_,_,_,_,x) -> x) <$> get
+getAliases = aliases <$> get
 
--- | Modify the options of a running bli application. 
-modifyOpts :: (BliSet t1, 
-               BliSet t2,
-               BliSet t3,
-               BliSet t4,
-               Alias alias)
+-- | Modify the configuration of a running bli application. 
+modifyConfig :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
  => (AppConfig -> AppConfig) -> Bli t1 t2 t3 t4 alias ()
-modifyOpts f = modify (\(x1,x2,x3,x4,x5,x6) -> (f x1, x2, x3, x4, x5, x6))
+modifyConfig f = modify (\bliCtx -> bliCtx { config = f (config bliCtx) } )
 
--- | Modify the program of a running bli application.
-modifyProgram :: (BliSet t1, 
-                  BliSet t2,
-                  BliSet t3,
-                  BliSet t4,
-                  Alias alias)
+-- | Modify the facts of a running bli application.
+modifyFacts :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
  => (t1 Clause -> t1 Clause) -> Bli t1 t2 t3 t4 alias ()
-modifyProgram f = modify (\(x1,x2,x3,x4,x5,x6) -> (x1, f x2, x3, x4, x4, x6))
+modifyFacts f = modify (\bliCtx -> bliCtx { facts = f (facts bliCtx) } )
 
 -- | Modify the schema of a running bli application.
-modifyRelations :: (BliSet t1, 
-                    BliSet t2,
-                    BliSet t3,
-                    BliSet t4,
-                    Alias alias)
+modifyRelations :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
  => (t2 SchemaEntry -> t2 SchemaEntry) -> Bli t1 t2 t3 t4 alias ()
-modifyRelations f = modify (\(x1,x2,x3,x4,x5,x6) -> (x, y, f z))
+modifyRelations f = modify (\bliCtx -> bliCtx { relations = f (relations bliCtx) } )
 
-modifyEntities :: (BliSet t1,
-                   BliSet t2,
-                   BliSet t3,
-                   BliSet t4,
-                   Alias alias)
+modifyEntities :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
  => (t3 RelDecl -> t3 RelDecl) -> Bli t1 t2 t3 t4 alias ()
-modifyEntities f = modify (\(x1,x2,x3,x4,x5,x6) -> ())
+modifyEntities f = modify (\bliCtx -> bliCtx { entities = f (entities bliCtx) } )
 
-modifyTypes :: (BliSet t1,
-                BliSet t2,
-                BliSet t3,
-                BliSet t4,
-                Alias alias)
+modifyTypes :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
  => (t4 TypeDecl -> t4 TypeDecl) ->
-modifyTypes = modify (\(x1,x2,x3,x4,x5,x6) -> ())
+modifyTypes = modify (\bliCtx -> bliCtx { types = f (types bliCtx) } )
 
-modifyAliases :: (BliSet t1,
-                  BliSet t2,
-                  BliSet t3,
-                  BliSet t4,
-                  Alias alias)
+modifyAliases :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
  => (alias -> alias) -> Bli t1 t2 t3 t4 t5 alias ()
-modifyAliases = modify (\(x1,x2,x4,x4,x5,x6) -> ())
+modifyAliases = modify (\bliCtx -> bliCtx { aliases = f (aliases bliCtx) } )
 
--- | Set the program of a running bli application.
-setProgram :: (BliSet t1, BliSet t2) => t1 Clause -> Bli t1 t2 ()
-setProgram val = modify (\(x,y,z,_,_,_) -> (x, val, z))
+-- | Set the facts of a running bli application.
+setFacts :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias) 
+ => t1 Clause -> Bli t1 t2 t3 t4 alias ()
+setFacts val = modify (\bliCtx -> bliCtx { facts = val } )
 
--- | Set the options of a running bli application
-setOpts :: (BliSet t1, BliSet t2) => AppConfig -> Bli t1 t2 ()
-setOpts val = modify (\(x,y,z,_,_,_) -> (val, y, z))
+-- | Set the config of a running bli application
+setConfig :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
+ => AppConfig -> Bli t1 t2 t3 t4 alias ()
+setConfig val = modify (\bliCtx -> bliCtx { config = val } )
 
--- | Set the schema of a running bli application
-setSchema :: (BliSet t1, BliSet t2) => t2 SchemaEntry -> Bli t1 t2 ()
-setSchema val = modify (\(x,y,z,_,_,_) -> (x, y, val))
+-- | Set the relations of a running bli application
+setRelations :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
+ => t2 SchemaEntry -> Bli t1 t2 t3 t4 alias ()
+setRelations val = modify (\bliCtx -> bliCtx { relations = val } )
+
+-- | Set the relations of a running bli application
+setEntities :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
+ => t2 SchemaEntry -> Bli t1 t2 t3 t4 alias ()
+setEntities val = modify (\bliCtx -> bliCtx { entities = val } )
+
+-- | Set the relations of a running bli application
+setTypes :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
+ => t2 SchemaEntry -> Bli t1 t2 t3 t4 alias ()
+setTypes val = modify (\bliCtx -> bliCtx { types = val } )
+
+setAliases :: (BliSet t1, BliSet t2, BliSet t3, BliSet t4, Alias alias)
+ => alias -> Bli t1 t2 t3 t4 alias ()
+setTypes val = (\bliCtx -> bliCtx { aliases = val } )
