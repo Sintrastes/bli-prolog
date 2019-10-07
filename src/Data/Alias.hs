a0900604a1dcb336fbe75448b7c8d0c2158b2c2e
Nathan Bedell <nbedell@tulane.edu>
2019-10-07 17:42:30 -0400

    Decided to keep the name "Alias"

--- src/Data/Alias.hs
+++ src/Data/Alias.hs
@@ -5,11 +5,11 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Control.Empty
 
-class (Traversable t, HasEmpty t) => AliasSet t where                                            
+class (Traversable t, HasEmpty t) => Alias t where                                            
     getPrimaryId' :: (t String) -> String -> Maybe String
     addNewAlias'  :: (t String) -> (String, String) -> Maybe (t String)
 
-instance AliasSet (Map String)  where
+instance Alias (Map String)  where
     getPrimaryId' store id = lookup id store
     addNewAlias' store (id1, id2) =
       case (getPrimaryId' store id1) of
