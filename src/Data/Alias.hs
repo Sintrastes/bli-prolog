1a89754d34b85c13913d49457fcc737ce8eb5bbe
Nathan Bedell <nbedell@tulane.edu>
2019-10-09 15:27:00 -0400

    Updated interface for Alias.

--- src/Data/Alias.hs
+++ src/Data/Alias.hs
@@ -4,12 +4,20 @@ module Data.Alias where
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Control.Empty
+import qualified Data.BliSet as BliSet
 
-class (Traversable t, HasEmpty t) => Alias t where                                            
-    getPrimaryId' :: (t String) -> String -> Maybe String
-    addNewAlias'  :: (t String) -> (String, String) -> Maybe (t String)
+-- t is a mapping for associating identifiers with their primary ids,
+-- and p is a container for looking up primary ids.
+class (Traversable t, HasEmpty t, BliSet p) => Alias t p where
+    addNewPrimaryId' :: (p String) -> String -> Maybe (p String)
+    getPrimaryId'    :: (p String) -> String -> Maybe String
+    addNewAlias'     :: (t String) -> (String, String) -> Maybe (t String)
 
-instance Alias (Map String)  where
+instance Alias (Map String) [] where
+    addNewPrimaryId' pids pid = 
+      case BliSet.tryInsert pid pids of
+        Right result -> Just result
+        Left  result -> Nothing
     getPrimaryId' store id = Map.lookup id store
     addNewAlias' store (id1, id2) =
       case (getPrimaryId' store id1) of
