f752415a482890f30e4d105ee6f4353519e6a5f8
Nathan Bedell <nbedell@tulane.edu>
2019-10-09 15:38:44 -0400

    Updated logic for our alias instance.

--- src/Data/Alias.hs
+++ src/Data/Alias.hs
@@ -4,24 +4,30 @@ module Data.Alias where
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Control.Empty
+import Data.BliSet (BliSet)
 import qualified Data.BliSet as BliSet
 
 -- t is a mapping for associating identifiers with their primary ids,
 -- and p is a container for looking up primary ids.
 class (Traversable t, HasEmpty t, BliSet p) => Alias t p where
-    addNewPrimaryId' :: (p String) -> String -> Maybe (p String)
-    getPrimaryId'    :: (p String) -> String -> Maybe String
-    addNewAlias'     :: (t String) -> (String, String) -> Maybe (t String)
+    addNewPrimaryId' :: (p String) -> (t String) -> String -> Maybe (p String)
+    getPrimaryId'    :: (p String) -> (t String) -> String -> Maybe String
+    addNewAlias'     :: (p String) -> (t String) -> (String, String) -> Maybe (t String)
 
 instance Alias (Map String) [] where
-    addNewPrimaryId' pids pid = 
+    addNewPrimaryId' pids _ pid = 
       case BliSet.tryInsert pid pids of
         Right result -> Just result
         Left  result -> Nothing
-    getPrimaryId' store id = Map.lookup id store
-    addNewAlias' store (id1, id2) =
-      case (getPrimaryId' store id1) of
+    getPrimaryId' pids store id = 
+      case addNewPrimaryId' pids store id of
+         -- id is already a primary id.
+         Nothing -> Just id
+         -- If id is not a primary id, look it up in the store.
+         Just _ -> Map.lookup id store
+    addNewAlias' pids store (id1, id2) =
+      case (getPrimaryId' pids store id1) of
          Just primaryId1 -> Just $ Map.insert id2 primaryId1 store
-         Nothing -> case (getPrimaryId' store id2) of
+         Nothing -> case (getPrimaryId' pids store id2) of
             Just primaryId2 -> Just $ Map.insert id1 primaryId2 store
             Nothing -> Nothing
