82bd1bf9b38ed318669f6d0bbfed8b38bcf67b7b
Nathan Bedell <nbedell@tulane.edu>
2019-10-16 16:53:10 -0400

    Started writing code to load modules in .bpl files.

--- src/Bli/Prolog/Modules.hs
+++ src/Bli/Prolog/Modules.hs
@@ -10,18 +10,53 @@ import Control.Monad.Bli
 import Bli.App.Config (getBliModuleData)
 import Data.Yaml
 import Bli.Util (groupClauses)
+import Data.List.Ordered
 
 -- | Helper function to load a bli program
 --   into the given context.
 --
 --   Note: I might want to put this somewhere else later.
 loadBliProgram :: BliProgram -> Bli ()
-loadBliProgram prog = undefined
+loadBliProgram prog = do
+  maybeModuleData <- liftIO $ getBliModuleData
+  case maybeModuleData of
+    Nothing -> error "Error parsing module file."
+    Just modData -> go prog [] modData
+ where go prog loadedMods modData = do
+          let (types, 
+               relations,
+               entities,
+               clauses,
+               moduleNames) 
+            = groupClauses prog
+          -- TODO: Do type-checking here.
+          
+          -- Add everything to the local context.
+          newTypes types
+          newRelations relations
+          newEntities entities
+          newClauses clauses
+                    
+          case moduleNames `subset` loadedMods of
+            True  -> return ()
+            False -> do
+              forM (\modName -> do
+                      let maybeModPath = lookup modName modData
+                      case maybeModPath of
+                        Nothing -> error "Module has not been declared."
+                        Just modPath -> do
+                          maybeLoadedMod <- loadBliProgramFromFile modPath
+                          case maybeLoadedMod of
+                            Nothing -> error "Error loading module."
+                            Just loadedMod -> do
+                              loadBliProgram loadedMod
+                   ) moduleNames
+
 -- | Helper function to load a Bli program from a file.
 --   Returns a boolean flag to indicate success/failure.
 -- 
 --   Note: Again, I may want to put this somewhere else later.
-loadBliProgramFromFile :: String -> Bli Bool
+loadBliProgramFromFile :: String -> Bli (Maybe BliProgram)
 loadBliProgramFromFile filePath = undefined
 
 -- | loads all of the modules imported in a bli prolog file.
